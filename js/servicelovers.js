650e2ad0abbbf56452bd59c1bffa254736e42905
Serviceloversweb <73524647+Serviceloversweb@users.noreply.github.com>
2020-11-17 11:42:20 +0100

    New texts and content

--- js/servicelovers.js
+++ js/servicelovers.js
@@ -28,7 +28,7 @@
 
   // Collapse Navbar
   var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 200) {
+    if ($("#mainNav").offset().top > 75) {
           $("#mainNav").addClass("navbar-shrink");
     } else {
           $("#mainNav").removeClass("navbar-shrink");
